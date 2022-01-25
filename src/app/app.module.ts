54701fc55d49a36a35524033004f1fd47b629c17
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-01-25 13:26:12 +0300

    generated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
+import { AttendaceComponent } from './components/attendace/attendace.component';
+import { CtaComponent } from './components/cta/cta.component';
+import { PartnersComponent } from './components/partners/partners.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroComponent,
+    AboutComponent,
+    AttendaceComponent,
+    CtaComponent,
+    PartnersComponent,
+    GalleryComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
