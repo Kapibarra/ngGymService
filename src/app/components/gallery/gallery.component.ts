10c70ac2fd058a7c5ab186105cf23207c1a98f54
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-01-27 16:32:03 +0300

    gallery imgs

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Gallery {
+  src: string;
+  id? : number,
+}
+
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.scss']
+  styleUrls: ['./gallery.component.scss'],
 })
 export class GalleryComponent implements OnInit {
+  images: Gallery[] = [
+    {
+      src: '../../../assets/img/gallery1.png',
+    },
+    {
+      src: '../../../assets/img/gallery2.png',
+    },
+    {
+      src: '../../../assets/img/gallery3.png',
+    },
+    {
+      src: '../../../assets/img/gallery4.png',
+    },
+    {
+      src: '../../../assets/img/gallery5.png',
+    },
+    {
+      src: '../../../assets/img/gallery6.png',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
