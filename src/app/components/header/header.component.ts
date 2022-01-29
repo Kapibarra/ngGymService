5cbab22b48163e8810f34b975c9065bc7b2fd017
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-01-29 18:28:50 +0300

    fixes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,30 +1,22 @@
 import { ViewportScroller } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
   mobile = false;
-
-  constructor(private viewportscroller: ViewportScroller) { 
-
-  }
-
+  constructor(private viewportscroller: ViewportScroller) {}
   ngOnInit(): void {
-    if (window.screen.width <= 968) { // 768px portrait
+    if (window.screen.width <= 968) {
+      // 768px portrait
       this.mobile = true;
       console.log('mobile', this.mobile);
-      
     }
-
   }
- onClickScroll(elementId: string):void {
-this.viewportscroller.scrollToAnchor(elementId);
-
- }
+  onClickScroll(elementId: string): void {
+    this.viewportscroller.scrollToAnchor(elementId);
+  }
 }
