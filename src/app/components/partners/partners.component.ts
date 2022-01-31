52337c4ae84463bcc4cd7a16252923c0d3b1ece1
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-01-31 15:52:44 +0300

    fixes

--- src/app/components/partners/partners.component.ts
+++ src/app/components/partners/partners.component.ts
@@ -3,36 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-partners',
   templateUrl: './partners.component.html',
-  styleUrls: ['./partners.component.scss']
+  styleUrls: ['./partners.component.scss'],
 })
 export class PartnersComponent implements OnInit {
+  // imageObject = [
 
-  imageObject = [{
-    image: '../../../assets/img/partners1.png',
-    thumbImage: '../../../assets/img/partners1.png',
-}, {
-  image: '../../../assets/img/partners2.png',
-  thumbImage: '../../../assets/img/partners2.png',
-}, {
-  image: '../../../assets/img/partners3.png',
-  thumbImage: '../../../assets/img/partners3.png',
-},{
-  image: '../../../assets/img/partners4.png',
-  thumbImage: '../../../assets/img/partners4.png',
-}, {
-  image: '../../../assets/img/partners5.png',
-  thumbImage: '../../../assets/img/partners5.png',
-},
-{
-  image: '../../../assets/img/partners6.png',
-  thumbImage: '../../../assets/img/partners6.png',
-}, {
-  image: '../../../assets/img/partners7.png',
-  thumbImage: '../../../assets/img/partners7.png',
-}];
-  constructor() { }
+  //   {
+  //     image: '../../../assets/img/partners5.png',
+  //     thumbImage: '../../../assets/img/partners5.png',
+  //   },
+  //   {
+  //     image: '../../../assets/img/partners7.png',
+  //     thumbImage: '../../../assets/img/partners7.png',
+  //   },
+  //   {
+  //     image: '../../../assets/img/partners6.png',
+  //     thumbImage: '../../../assets/img/partners6.png',
+  //   },
+    
+  // ];
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
