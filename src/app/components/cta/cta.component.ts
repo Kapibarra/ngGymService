0cdec2a10d08a63f3d723fe4ac00a288e30dafbc
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-01-31 18:17:57 +0300

    fix burger

--- src/app/components/cta/cta.component.ts
+++ src/app/components/cta/cta.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cta.component.scss']
 })
 export class CtaComponent implements OnInit {
-
+  mobile = false;
+  desktop = false;
   constructor() { }
 
   ngOnInit(): void {
+    if (window.screen.width >= 968) {
+      // 768px portrait
+      this.desktop = true;
+      console.log('desktop', this.desktop);
+    }
+    if (window.screen.width <= 968) {
+      // 768px portrait
+      this.mobile = true;
+      console.log('mobile', this.mobile);
+    }
   }
 
 }
