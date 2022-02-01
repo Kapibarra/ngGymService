c17d98cb0a15bf2dedcab4124a6a36cd4243a551
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-02-01 11:13:12 +0300

    fix php mailer

--- src/handler.php
+++ src/handler.php
@@ -9,7 +9,7 @@ require 'php/PHPMailer-5.2.28/src/SMTP.php';
 
 $mail = new PHPMailer(true);
 $mail_subject = 'Заявка с сайта';
-$mail_to_email = 'info@fitserv.ru'; // your email
+$mail_to_email = 'fitservmsk@gmail.com'; // your email
 $mail_to_name = 'Fitserv.ru';
 
 try {
@@ -28,7 +28,7 @@ try {
 	$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 	$mail->Port = 465; // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-	$mail->setFrom($mail->Username, 'qwerty'); // Your email
+	$mail->setFrom($mail->Username, 'Fitserv.ru'); // Your email
 	$mail->addAddress($mail_to_email, $mail_to_name); // Add a recipient на какой емейл отправить!! проверить!!!!
 
 	for($ct=0; $ct<count($_FILES['file_attach']['tmp_name']); $ct++) {
@@ -41,14 +41,15 @@ try {
 	$mail->Subject = $mail_subject;
 	$mail->Body = '
 		
-		<strong>Телефон:</strong> ' . $mail_from_phone . '<br>
-		<strong>Имя:</strong> ' . $mail_from_name; 
+		<strong>Phone:</strong> ' . $mail_from_phone . '<br>
+		<strong>Name:</strong> ' . $mail_from_name; 
 	$mail->Send();
+
 	echo '<script>
 	alert("Your message has been sent!")
 	</script>';
 
 } catch (Exception $e) {
-	// echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+	echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 
 }
