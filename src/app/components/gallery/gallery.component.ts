7b65a25324263dc0cb88a215887bd7d1ae1b9e41
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-02-01 18:32:16 +0300

    new gallery

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -30,6 +30,12 @@ export class GalleryComponent implements OnInit {
     {
       src: '../../../assets/img/gallery6.png',
     },
+    {
+      src: '../../../assets/img/gallery7.png',
+    },
+    {
+      src: '../../../assets/img/gallery8.png',
+    },
   ];
 
   constructor() {}
