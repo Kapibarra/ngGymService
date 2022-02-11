a784932ed3220d21429f49048022de78dcf29829
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-02-11 16:43:17 +0300

    fix footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  mobile = false;
   constructor() { }
 
   ngOnInit(): void {
+    if (window.screen.width <= 968) {
+      // 768px portrait
+      this.mobile = true;
+      console.log('mobile', this.mobile);
+    }
   }
 
 }
